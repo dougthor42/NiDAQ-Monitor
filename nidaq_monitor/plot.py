c463f4a882241b1872fe351b2e331d282d93f235
Douglas Thor <dougthor42@users.noreply.github.com>
2015-01-09 17:49:16 -0800

    Got major gridlines working, yay!

--- nidaq_monitor/plot.py
+++ nidaq_monitor/plot.py
@@ -29,6 +29,8 @@ __version__ = "v0.1.0"
 
 
 wx.MAGENTA = wx.Colour(255, 0, 255, 255)
+wx.GREY = wx.Colour(192, 192, 192, 255)
+wx.GRAY = wx.GREY
 
 
 #class ExamplePlot(PlotCanvas.PlotCanvas):
@@ -113,7 +115,7 @@ class ScrollingListPlot(wx.Panel):
     def __init__(self,
                  parent,
                  num_data_sets=3,
-                 max_pts=15,
+                 max_pts=50,
                  displayed_pts=5,
                  timestamp_x=False,
                  moving_avg=False,
@@ -207,10 +209,14 @@ class ScrollingListPlot(wx.Panel):
                                               )
 
         self.canvas.InitAll()
-        self.canvas.GridUnder = Grid((2, 1),
-                                     Size=4,
-                                     Color="Grey",
-                                     Cross=True
+#        self.canvas.GridUnder = Grid((2, 1),
+#                                     Size=4,
+#                                     Color="Grey",
+#                                     Cross=True
+#                                     )
+        self.canvas.GridUnder = Grid((5, 6),
+                                     num_minor_x_lines=0,
+                                     num_minor_y_lines=0,
                                      )
 
         self.hbox = wx.BoxSizer(wx.HORIZONTAL)
@@ -227,7 +233,7 @@ class ScrollingListPlot(wx.Panel):
 
         Start the timer
         """
-        self.timer.Start(250)
+        self.timer.Start(500)
 
     def on_timer(self, event):
         """
@@ -332,10 +338,10 @@ class ScrollingListPlot(wx.Panel):
 
         self.canvas.Update()
         self.canvas.ZoomToBB()
-        print(self.canvas.BoundingBox)
 
     def _add_limit_lines(self):
         """ Creates the limit lines and adds them to the plot """
+        # TODO: Could probably use FloatCanvas.GridOver to achieve this...
         if self.uwl is not None:
             uwl_pts = ((self.x_data[0], self.uwl),
                        (self.x_data[-1], self.uwl))
@@ -383,7 +389,7 @@ class ScrollingListPlot(wx.Panel):
         return
 
 # TODO: Update this to be lines rather than dots
-class Grid(object):
+class DotGrid(object):
     """
     An example of a Grid Object -- it is set on the FloatCanvas with one of:
 
@@ -474,6 +480,155 @@ class Grid(object):
                 else:
                     for xy in Points:
                         dc.DrawCircle(xy[0], xy[1], radius)
+
+
+# TODO: Update this to be lines rather than dots
+class Grid(object):
+    """
+    An example of a Grid Object -- it is set on the FloatCanvas with one of:
+
+    FloatCanvas.GridUnder = Grid
+    FloatCanvas.GridOver = Grid
+
+    It will be drawn every time, regardless of the viewport.
+
+    In its _Draw method, it computes what to draw, given the ViewPortBB
+    of the Canvas it's being drawn on.
+
+    """
+    def __init__(self,
+                 spacing,
+                 num_minor_x_lines=0,
+                 num_minor_y_lines=0,
+                 major_lw=1,
+                 major_color=wx.GREY,
+                 major_style=wx.SOLID,
+                 minor_lw=1,
+                 minor_color=wx.GREY,
+                 minor_style=wx.SOLID,
+                 ):
+
+        self.spacing = np.array(spacing, np.float)
+        self.spacing.shape = (2,)
+        self.num_minor_x_lines = num_minor_x_lines
+        self.num_minor_y_lines = num_minor_y_lines
+        self.major_lw = major_lw
+        self.major_color = major_color
+        self.major_style = major_style
+        self.minor_lw = minor_lw
+        self.minor_color = minor_color
+        self.minor_style = minor_style
+
+    def calc_lines(self, canvas):
+        """
+        Calculate where the gridlines are.
+
+        Returns 4 lists of 4-tuples (x1, y1, x2, y2) - a pair for major lines
+        and a 2nd pair for minor lines - to be used with the dc.DrawLineList()
+        method.
+        """
+        ViewPortBB = canvas.ViewPortBB
+
+        spacing = self.spacing
+
+        minx, miny = np.floor(ViewPortBB[0] / spacing) * spacing
+        maxx, maxy = np.ceil(ViewPortBB[1] / spacing) * spacing
+
+        major_x = np.arange(minx, maxx + spacing[0], spacing[0])
+        major_y = np.arange(miny, maxy + spacing[1], spacing[1])
+
+        # TODO: calculate the minor gridlines and return those
+        minor_x = None
+        minor_y = None
+
+        if self.num_minor_x_lines == 0:
+            minor_x = None
+        if self.num_minor_y_lines == 0:
+            minor_y = None
+
+        return major_x, major_y, minor_x, minor_y
+
+    def convert_lines(self, canvas, lines, axis=0):
+        """
+        Takes the gridlines calcualted by calc_lines and converts them
+        from World coordinates to pixel coordinates and formats them in
+        lists of (x1, y1, x2, y2) tuples for use with the dc.DrawLineList
+        method.
+
+        canvas:
+            The canvas that is painted on
+
+        lines:
+            1D NumPy array denoting the locations of the gridlines for the
+            given axis
+
+        axis:
+            0: x-axis, drawing vertical lines
+            1: y-axis, drawing horizontal lines
+        """
+        # Get the BB extents
+        bb = canvas.ViewPortBB
+
+        if axis == 0:       # x axis
+            min_coord, max_coord = bb[:, 1]     # use y-axis extents
+        elif axis == 1:     # y axis
+            min_coord, max_coord = bb[:, 0]     # use x-axis extents
+        else:
+            raise ValueError("axis must be 0 (x) or 1 (y)")
+
+        N = lines.size
+
+        # TODO: Make more NumPythonic
+        start_coords = np.zeros((2, N), dtype=np.float64)
+        if axis == 1:
+            start_coords[:][0] = min_coord
+            start_coords[:][1] = lines
+        else:
+            start_coords[:][0] = lines
+            start_coords[:][1] = min_coord
+
+        end_coords = np.zeros((2, N), dtype=np.float64)
+        if axis == 1:
+            end_coords[:][0] = max_coord
+            end_coords[:][1] = lines
+        else:
+            end_coords[:][0] = lines
+            end_coords[:][1] = max_coord
+
+        start_coords = canvas.WorldToPixel(start_coords.T)
+        end_coords = canvas.WorldToPixel(end_coords.T)
+
+        # TODO: Make more NumPythonic
+        pixel_lines = []
+        for start, end in zip(start_coords, end_coords):
+            pixel_lines.append((start[0], start[1], end[0], end[1]))
+
+        pixel_lines = np.array(pixel_lines, dtype=np.float64)
+
+        return pixel_lines
+
+    def _Draw(self, dc, canvas):
+        """ Actually draw the lines """
+
+        # Calculate the lines
+        major_x, major_y, _, _ = self.calc_lines(canvas)
+
+        # Need to convert to Pixel Coordinates to use dc.DrawLineList
+        major_x = self.convert_lines(canvas, major_x, 0)
+        major_y = self.convert_lines(canvas, major_y, 1)
+#        minor_x = self.convert_lines(canvas, minor_y, 0)
+#        minor_y = self.convert_lines(canvas, minor_y, 0)
+
+        # Define the pens.
+        major_pen = wx.Pen(self.major_color, self.major_lw, self.major_style)
+#        minor_pen = wx.Pen(self.minor_color, self.minor_lw, self.minor_style)
+
+        # Draw the lines on the dc.
+        dc.DrawLineList(major_x, major_pen)
+        dc.DrawLineList(major_y, major_pen)
+#        dc.DrawLineList(minor_x, major_pen)
+#        dc.DrawLineList(minor_y, major_pen)
+
 
 
 class _TestFrame(wx.Frame):
